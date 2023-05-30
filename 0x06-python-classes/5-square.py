8f672a4caa40678b00df6fd149f32c4779fd53a1
Henry-Waswa <55430679+Henry-Waswa@users.noreply.github.com>
2023-05-30 18:52:02 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Defines the square class"""
 
 
 class Square:
-    """Represent a square."""
-    def __init__(self, size):
-        """Initialize a new square.
-        
-        Args:
-            size (int): The size of the new square.
-        """
-        self.size = size
-   
+    """Square class. Has a size""" 
+    def __init__(self, size=0):
+        """Initialize Square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        """Returns area of the square"""
+        return self.__size * self.__size
+    
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        """Return size of the square"""
+        return self.__size
     
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """Set size of the square"""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
-    
+   
     def my_print(self):
-        """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
         if self.__size == 0:
-            print("")
+            print()
+        else:
+            str = '#' * self.__size
+            for x in range(self.__size):
+                print(str)
